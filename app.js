7a10e5003dc712043f2e6153adc314bb9e1f4285
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 17:54:05 +0700

    js lab 136

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// write code here
+// จงหาคะแนนเฉลี่ยแบบถ่วงน้ำหนัก
+
+const scores = [
+  { score: 90, subject: "HTML", weight: 0.2 },
+  { score: 95, subject: "CSS", weight: 0.3 },
+  { score: 85, subject: "JavaScript", weight: 0.5 },
+];
+
+function weightScore(scores) {
+  let totalScore = 0;
+  let totalWeight = 0;
+
+  for (i = 0; i < scores.length; i++) {
+    totalScore += scores[i].score * scores[i].weight;
+    totalWeight += scores[i].weight;
+  }
+
+  const average = totalScore / totalWeight;
+  return average;
+}
+
+console.log(weightScore(scores));
+
+// expected result: 89
